ed86c7eaf72ade12847056af15b2c27901d0330a
Kevin Luu <this.kevinluu@gmail.com>
2021-11-04 12:04:16 -0400

    deploy eks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = "~>0.12"
   backend "s3" {
     bucket = "udacity-capstone-110421"
     key    = "udacity-capstone.tfstate"
@@ -22,7 +23,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 2.48"
 
-  name = "eks-circleci-vpc"
+  name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
   azs                  = slice(data.aws_availability_zones.azs.names, 0, 2)
@@ -49,10 +50,10 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 12.2"
+  version = "~> 17.23.0"
 
+  cluster_version = "1.21"
   cluster_name    = local.cluster_name
-  cluster_version = "1.17"
   subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
@@ -76,7 +77,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
 }
 
 output "kubectl_config" {
